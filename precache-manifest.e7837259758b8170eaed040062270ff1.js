1877cdb6abe3a041fbab57c5559f201eeb3b6c3c
markozromero <marcosrrm15_v3@hotmail.com>
2019-01-02 19:17:14 -0600

    Updates

---
+++ precache-manifest.e7837259758b8170eaed040062270ff1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa9d25a1e9cb2ec437fa",
+    "url": "/crunchyroll-competitor/static/js/runtime~main.fa9d25a1.js"
+  },
+  {
+    "revision": "5d065fd0116a83e30bc5",
+    "url": "/crunchyroll-competitor/static/js/main.5d065fd0.chunk.js"
+  },
+  {
+    "revision": "98f15cd8ec8026638d3f",
+    "url": "/crunchyroll-competitor/static/js/1.98f15cd8.chunk.js"
+  },
+  {
+    "revision": "5d065fd0116a83e30bc5",
+    "url": "/crunchyroll-competitor/static/css/main.b6bb567b.chunk.css"
+  },
+  {
+    "revision": "b719a468371a25a726537da3f8c42e2e",
+    "url": "/crunchyroll-competitor/index.html"
+  }
+];
